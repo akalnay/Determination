0e4160cd15c7fba2584a86ada5010121c911df39
Alejandro Kalnay <alejandrok@att.net>
2020-10-05 06:48:00 -0400

    Moved the DirectionManager class to a separate file.

---
+++ Determination.Tests/ValueProvider/DirectionManager.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////
+// Copyright (c) Alejandro Kalnay                     //
+// License: GNU GPLv3                                 //
+////////////////////////////////////////////////////////
+
+using System;
+using System.Linq;
+
+namespace Determination.Tests
+{
+    public enum Direction { Up = 1, Right, Down, Left }
+
+    /// <summary>
+    /// This class is just for demonstration purposes.  The <see cref="GetNextRandomDirection"/> method
+    /// returns a random <see cref="Direction"/> value when invoked.
+    /// </summary>
+    public sealed class DirectionManager
+    {
+        private static readonly Random _RANDOM = new Random();
+
+        /// <summary>
+        /// Returns a random <see cref="Direction"/>
+        /// </summary>
+        /// <returns>A random <see cref="Direction"/></returns>
+        public static Direction GetNextRandomDirection() => (Direction)_RANDOM.Next((int)Min<Direction>(), Count<Direction>() + 1);
+
+        private static T Min<T>() where T : struct, Enum => Enum.GetValues(typeof(T)).Cast<T>().Min();
+
+        private static int Count<T>() where T : struct, Enum => Enum.GetNames(typeof(T)).Length;
+    }
+}
--- Determination.Tests/ValueProvider/ValueProviderTests.cs
+++ Determination.Tests/ValueProvider/ValueProviderTests.cs
@@ -4,36 +4,24 @@
 ////////////////////////////////////////////////////////
 
 using NUnit.Framework;
-using System;
-using System.Linq;
 
 namespace Determination.Tests
 {
     // Sanity Check tests for the ValueProvider class
     public sealed class ValueProviderTests
     {
-        private enum Direction { Up = 1, Right, Down, Left }
-
-        private static readonly Random _RANDOM = new Random();
-
         [Test]
         [Category("ValueProvider Tests")]
         public void WhenTheValueProviderClassIsInstantiated_ThenAnExceptionIsNotThrown()
         {
-            Assert.DoesNotThrow(() => _ = ValueProvider.Create(GetNextRandomDirection));
+            Assert.DoesNotThrow(() => _ = ValueProvider.Create(DirectionManager.GetNextRandomDirection));
         }
 
         [Test]
         [Category("ValueProvider Tests")]
         public void WhenTheValueProviderValuePropertyIsRetrieved_ThenAnExceptionIsNotThrown()
         {
-            Assert.DoesNotThrow(() => _ = ValueProvider.Create(GetNextRandomDirection).Value);
+            Assert.DoesNotThrow(() => _ = ValueProvider.Create(DirectionManager.GetNextRandomDirection).Value);
         }
-
-        private static Direction GetNextRandomDirection() => (Direction)_RANDOM.Next((int)Min<Direction>(), Count<Direction>() + 1);
-
-        private static T Min<T>() where T : struct, Enum => Enum.GetValues(typeof(T)).Cast<T>().Min();
-
-        private static int Count<T>() where T : struct, Enum => Enum.GetNames(typeof(T)).Length;
     }
 }
